0718ddde2fe8dcdd0b36ea209db4ad7eb366519e
Samir Soormally <samirsoormally@gmail.com>
2019-03-11 09:36:46 +0000

    chore: Vue: updated App.vue to call Exercise Component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ExerciseComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ExerciseComponent from './components/ExerciseComponent.vue';
+
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ExerciseComponent
   }
 }
 </script>
@@ -18,10 +18,8 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  margin-bottom: 60px;
 }
 </style>
