991225b1510a4a7581b017324ec189cc643c1c72
Samir Soormally <samirsoormally@gmail.com>
2019-03-11 08:57:43 +0000

    feat: app connects to db on serve

--- server/app.js
+++ server/app.js
@@ -1,13 +1,23 @@
+import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import exercises from './routes/exercises';
+import models, { connectDb } from './models';
 import routes from './routes';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+// passes the models as context to all of the Express routes
+app.use(async (req, res, next) => {
+  req.context = {
+    models
+  };
+  next();
+});
 
 app.use('/exercises', routes.exercises);
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const app = require('./app')
+import models, { connectDb } from './models';
 
-app.listen(5000, () =>
-  console.log(`Exercise Logger API app listening on port 5000`),
-);
+connectDb().then(async () => {
+  app.listen(process.env.API_PORT, () =>
+    console.log(`Example app listening on port ${process.env.API_PORT}!`),
+  );
+});
